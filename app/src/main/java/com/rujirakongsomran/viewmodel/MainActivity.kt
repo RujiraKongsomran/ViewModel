524d894b1bbe7fe8102df1548bb37e8b87a65b09
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-09-10 14:27:45 +0700

    finish

--- app/src/main/java/com/rujirakongsomran/viewmodel/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/viewmodel/MainActivity.kt
@@ -2,10 +2,21 @@ package com.rujirakongsomran.viewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LifecycleOwner {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
+        button.setOnClickListener {
+            viewModel.addNumber()
+            textView.text = viewModel.number.toString()
+        }
     }
 }
